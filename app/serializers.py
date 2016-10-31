77cc7c255cb80bdc1976124f234b02608a50f4db
andela-jmuli <joseph.muli@andela.com>
2016-10-31 23:56:15 +0300

    [Feature] Add model serializers Add serializers for users, bucketlists and bucketlist items

---
+++ app/serializers.py
@@ -0,0 +1,24 @@
+from flask_restful import fields
+
+
+users_serializer = {
+    "id" : fields.Integer,
+    "username": fields.String
+}
+
+bucketlists_serializer = {
+    "id" : fields.Integer,
+    "name" : fields.String,
+    "date_created" : fields.DateTime,
+    "date_modified" : fields.DateTime,
+    "created_by" : fields.Integer
+}
+
+bucketlist_items_serializer = {
+    "id" : fields.Integer,
+    "name" : fields.String,
+    "bucketlist_id" : fields.Integer,
+    "date_created" : fields.DateTime,
+    "date_modified" : fields.DateTime,
+    "is_done" : fields.Boolean
+}
