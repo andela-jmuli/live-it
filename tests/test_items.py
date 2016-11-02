b2b9bebe1667104e03d0ed7bb338608fc8f95007
andela-jmuli <joseph.muli@andela.com>
2016-11-02 15:23:04 +0300

    [Chore] Add requirements Add file for item resource tests

---
+++ tests/test_items.py
@@ -0,0 +1,16 @@
+# from tests import SuperTestCase
+
+
+# class TestItems(SuperTestCase):
+
+#     def test_item_creation(self):
+#         pass
+
+#     def test_duplicate_item(self):
+#         pass
+
+#     def test_editing_an_item(self):
+#         pass
+
+#     def test_deletion_of_an_item(self):
+#         pass
